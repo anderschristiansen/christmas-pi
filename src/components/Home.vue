bc64d9e49eccfca79baa94cc92ef2fb1084c5f22
Anders Buron Christiansen <abu@miracle.dk>
2020-11-24 14:47:53 +0100

    cleaned code

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,21 @@
 <template>
-    <div>
-    </div>
+  <v-container fluid>
+    <v-row align="center">
+      <v-col cols="12">
+        <div class="text-center">
+          <div class="my-2">
+            <v-btn color="primary" dark>
+              Normal Button
+            </v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
